bcbbcd77528bafb30532608615cf0f54223783f8
bcweaver <brianweaver@gmail.com>
2020-06-23 16:02:07 -0400

    Fix some annotations, squash all request headers

--- src/Plugin/api_proxy/ResearchBpcrcOsuEdu.php
+++ src/Plugin/api_proxy/ResearchBpcrcOsuEdu.php
@@ -5,15 +5,17 @@ use Drupal\api_proxy\Plugin\api_proxy\HttpApiCommonConfigs;
 use Drupal\api_proxy\Plugin\HttpApiPluginBase;
 use Drupal\Core\Form\SubformStateInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Drupal\Core\Logger;
+use Drupal\Core\Logger\RfcLogLevel;
 
 /**
  * The Example API.
  *
  * @HttpApi(
  *   id = "asc-bpcrc",
- *   label = @Translation("Example API"),
- *   description = @Translation("Proxies requests to the Example API."),
- *   serviceUrl = "https://research.bpcrc.osu.edu/",
+ *   label = @Translation("research.bpcrc.osu.edu fetcher"),
+ *   description = @Translation("Proxies requests research.bpcrc.osu.edu."),
+ *   serviceUrl = "http://research.bpcrc.osu.edu/",
  * )
  */
 final class ResearchBpcrcOsuEdu extends HttpApiPluginBase {
@@ -24,8 +26,8 @@ final class ResearchBpcrcOsuEdu extends HttpApiPluginBase {
    * {@inheritdoc}
    */
   public function addMoreConfigurationFormElements(array $form, SubformStateInterface $form_state): array {
-    $form['auth_token'] = $this->authTokenConfigForm($this->configuration);
-    $form['more_stuff'] = ['#type' => 'textfield', '#title' => $this->t('Extra config')];
+    // $form['auth_token'] = $this->authTokenConfigForm($this->configuration);
+    // $form['more_stuff'] = ['#type' => 'textfield', '#title' => $this->t('Extra config')];
     return $form;
   }
 
@@ -34,7 +36,12 @@ final class ResearchBpcrcOsuEdu extends HttpApiPluginBase {
    */
   protected function calculateHeaders(array $headers): array {
     // Modify & add new headers. Here you can add the auth token.
-    return $new_headers;
+    // return $new_headers;
+    \Drupal::logger('asc_cors_proxy')
+    ->log(RfcLogLevel::ERROR, print_r(array_keys($headers), true), array());
+
+    // return $headers;
+    return array();
   }
 
   /**
